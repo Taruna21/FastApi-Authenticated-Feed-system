2aef9d12fb0d0b1a962360937e55a00897f08ac1
Taruna <74425034+Taruna21@users.noreply.github.com>
2022-06-20 15:19:56 +0530

    Add files via upload

--- blog/main.py
+++ blog/main.py
@@ -1,77 +1,12 @@
-from codecs import raw_unicode_escape_decode
-import email
-from turtle import title
-from typing import List 
-from unittest.mock import NonCallableMock
-from click import password_option
-from fastapi import FastAPI, Depends , status, Response , HTTPException
-from . import schemas, models
-from .database import SessionLocal, engine 
-from sqlalchemy.orm import Session 
-
-
-import blog
+from fastapi import FastAPI
+from . import  models
+from .database import engine
+from  .routers import blog, user, authentication
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
-
-def create(request : schemas.Blog , db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-
-@app.get('/blog' , response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
-@app.put("/blog/update/{id}")
-def updateBlog(id, request:schemas.Blog, db:Session=Depends(get_db)):
-   blog= db.query(models.Blog).filter(models.Blog.id == id).first()
-   
-   if not blog:
-             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'blog with id {id} not found')
-   else:
-    db.query(models.Blog).filter(models.Blog.id == id).update(request.dict())
-
-    db.commit()
-    db.refresh(blog)
-
-    return blog
-
-@app.get("/blog/{id}", status_code=200 )
-def show(id, response: Response, db:Session=Depends(get_db)):
-    blog= db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the id {id} not available")
-    
-
-    return blog
-
-
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session=Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    return 'done'
-
-@app.post('/user')
-def create_user(request : schemas.User , db: Session = Depends(get_db)):
-    new_user = models.User(name= request.name, email=request.email, password = request.password)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from blog import  models
-from blog.database import engine
-from blog.routers import blog, user, authentication
+from . import  models
+from  database import engine
+from  routers import blog, user, authentication
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-app.include_router(authentication.routers)
+app.include_router(authentication.router)
 app.include_router(blog.routers)
 app.include_router(user.routers)
